905575774b4baf5db6c510bfc23512791be52292
Danilo <danilorincon04@gmail.com>
2023-05-11 14:26:21 -0400

    nmwmeinwd

---
+++ EHS_may_11/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  EHS_may_11
+//
+//  Created by Thayer on 5/11/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
