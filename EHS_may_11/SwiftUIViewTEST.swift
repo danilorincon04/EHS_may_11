aa64bbe1e1e589d2b96a4c671a6118188398b1c6
Danilo <danilorincon04@gmail.com>
2023-05-11 14:28:58 -0400

    test

---
+++ EHS_may_11/SwiftUIViewTEST.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIViewTEST.swift
+//  EHS_may_11
+//
+//  Created by Thayer on 5/11/23.
+//
+
+import SwiftUI
+
+struct SwiftUIViewTEST: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct SwiftUIViewTEST_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViewTEST()
+    }
+}
